583046ff5629c7801928bdd2a66b3c6349e71a70
chris_using_windows <achinga.chris@gmail.com>
2023-06-14 21:53:08 +0300

    search form

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,5 +1,17 @@
+import { useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+    console.log(e.target.value);
+  };
+
   return (
     <div className='card mb-4'>
       <div className='card-header'>Search</div>
@@ -11,14 +23,16 @@ const SearchBar = () => {
             placeholder='Enter search term...'
             aria-label='Enter search term...'
             aria-describedby='button-search'
+            value={searchTerm}
+            onChange={handleChange}
           />
-          <button className='btn btn-primary' id='button-search' type='button'>
+          <button className='btn btn-primary' id='button-search' type='button' onClick={handleSearch}>
             Go!
           </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
